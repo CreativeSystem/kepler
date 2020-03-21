45ba9a86f0bb779c77000edaff2a3e9973fcf096
leoferreiralima <leoferreiralima@yahoo.com>
2020-03-21 14:54:47 -0300

    tests ci

--- backend/keplerapi/keplerapi/settings.py
+++ backend/keplerapi/keplerapi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 try:
     load_dotenv(dotenv_path=".env")
 except:
-    print("")
+    pass
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
